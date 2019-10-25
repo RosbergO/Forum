da4b163d490ba00ba7bb3457a7c6d54117ed7b93
Oscar Rosberg <oscros-7@student.ltu.se>
2019-10-25 17:43:01 +0200

    Fixed all the shitz for routing that was wronK

--- Forum/Controllers/CategoryController.cs
+++ Forum/Controllers/CategoryController.cs
@@ -18,14 +18,13 @@ namespace Forum.Controllers
         {
             return View();
         }
-        public IActionResult Posts()
-        {
-            return View();
-        }
+   
 
-        public IActionResult ViewTest(int i)
+        public IActionResult Posts(int id)
         {
-            List<TblPosts> postList = SelectWithDataSet(1);
+            
+            List<TblPosts> postList = SelectWithDataSet(id);
+            ViewBag.postList = postList;
 
             return View(postList);
         } 
@@ -51,6 +50,7 @@ namespace Forum.Controllers
                     //postList.Add(new TblPosts(1, "Oscar", "HEj allihopA",DateTime.Now, 1, 1 ));    
                     // Console.WriteLine("Product: {0,-35} Total: {1,2}", rdr["ProductName"], rdr["Total"]);
                 }
+                cmd.Dispose();
             }
             catch (Exception e)
             {
@@ -69,61 +69,6 @@ namespace Forum.Controllers
                 }
             }
             return postList;
-        }
-
-        public void RunStoredProcParams(int i)
-        {
-            SqlConnection conn = null;
-            SqlDataReader rdr = null;
-
-            // typically obtained from user
-            // input, but we take a short cut
-            int custId = i;
-
-            try
-            {
-                // create and open a connection object
-                conn = new
-                    SqlConnection("Server=(local);DataBase=Northwind;Integrated Security=SSPI");
-                conn.Open();
-
-                // 1. create a command object identifying
-                // the stored procedure
-                SqlCommand cmd = new SqlCommand(
-                    "CustOrderHist", conn);
-
-                // 2. set the command object so it knows
-                // to execute a stored procedure
-                cmd.CommandType = CommandType.StoredProcedure;
-
-                // 3. add parameter to command, which
-                // will be passed to the stored procedure
-                cmd.Parameters.Add(
-                    new SqlParameter("@CustomerID", custId));
-
-                // execute the command
-                rdr = cmd.ExecuteReader();
-
-                // iterate through results, printing each to console
-                while (rdr.Read())
-                {
-                    Console.WriteLine(
-                        "Product: {0,-35} Total: {1,2}",
-                        rdr["ProductName"],
-                        rdr["Total"]);
-                }
-            }
-            finally
-            {
-                if (conn != null)
-                {
-                    conn.Close();
-                }
-                if (rdr != null)
-                {
-                    rdr.Close();
-                }
-            }
         }
     }
 
