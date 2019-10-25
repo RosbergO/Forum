ffe699b3cb91b81f00420a538a5806f54b091a55
Oscar Rosberg <oscros-7@student.ltu.se>
2019-10-25 14:46:53 +0200

    Added autogenerated View for posts

--- Forum/Controllers/CategoryController.cs
+++ Forum/Controllers/CategoryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,5 +21,62 @@ namespace Forum.Controllers
         {
             return View();
         }
+
+        public void RunStoredProcParams(int i)
+        {
+            SqlConnection conn = null;
+            SqlDataReader rdr = null;
+
+            // typically obtained from user
+            // input, but we take a short cut
+            int custId = i;
+
+            try
+            {
+                // create and open a connection object
+                conn = new
+                    SqlConnection("Server=(local);DataBase=Northwind;Integrated Security=SSPI");
+                conn.Open();
+
+                // 1. create a command object identifying
+                // the stored procedure
+                SqlCommand cmd = new SqlCommand(
+                    "CustOrderHist", conn);
+
+                // 2. set the command object so it knows
+                // to execute a stored procedure
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // 3. add parameter to command, which
+                // will be passed to the stored procedure
+                cmd.Parameters.Add(
+                    new SqlParameter("@CustomerID", custId));
+
+                // execute the command
+                rdr = cmd.ExecuteReader();
+
+                // iterate through results, printing each to console
+                while (rdr.Read())
+                {
+                    Console.WriteLine(
+                        "Product: {0,-35} Total: {1,2}",
+                        rdr["ProductName"],
+                        rdr["Total"]);
+                }
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+            }
+        }
     }
+
 }
+
