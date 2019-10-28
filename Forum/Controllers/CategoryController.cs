948e2572cf819fb882dcecec80a4a6e0799d335a
Oscar Rosberg <oscros-7@student.ltu.se>
2019-10-28 17:35:01 +0100

    Made index more scaleable but also more ugly jajaja

--- Forum/Controllers/CategoryController.cs
+++ Forum/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System.Data.SqlClient;
 using System.Data;
 using Forum.Models;
+using Microsoft.AspNetCore.Http;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -17,6 +18,46 @@ namespace Forum.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Create(string id)
+        {
+            if (HttpContext.Session.GetInt32("ID") != 4)
+            {
+                return RedirectToAction("Category", "posts");
+            }
+            int category = TblCategories.GetCategory(id);
+            ViewBag.categoryID = category;
+            ViewBag.category = id;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TblCategories category)
+        {
+            if(HttpContext.Session.GetInt32("ID") != 4)
+            {
+                return RedirectToAction("Category", "posts");
+            }
+            string message = "";
+            ViewBag.message = message;
+            TblCategories.InsertCategory(category.CaName.ToString(), out message);
+            return RedirectToAction("Posts/" + category.CaName, "Category");
+        }
+        public IActionResult Read()
+        {
+            List<TblCategories> categories = TblCategories.GetCategories();
+            return PartialView(categories);
+            /*
+            TblCategories category = TblCategories.GetCategory(id);
+            ViewBag.category = TblCategories.GetCategory(category.CaName);
+            foreach (TblComments item in ViewBag.comments)
+            {
+                users[item.CoAuthor] = TblUser.GetUserFromID(item.CoAuthor).UsName;
+            }
+            ViewBag.users = users;
+            return View(post);
+            */
         }
 
         public IActionResult Posts(int id)
--- Forum/Models/TblCategories.cs
+++ Forum/Models/TblCategories.cs
@@ -55,6 +55,83 @@ namespace Forum.Models
             }
             return output;
         }
+
+        internal static List<TblCategories> GetCategories()
+        {
+                List<TblCategories> output = new List<TblCategories>();
+                SqlConnection conn = null;
+                SqlDataReader rdr = null;
+
+                try
+                {
+                    conn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=MonsterPlan;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("spGetCategories", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                    output.Add(new TblCategories()
+                    {
+                        CaName = rdr["Ca_Name"].ToString(), CaId = int.Parse(rdr["Ca_ID"].ToString())
+                    });
+                    }
+                    cmd.Dispose();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                finally
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                    if (rdr != null)
+                    {
+                        rdr.Close();
+                    }
+                }
+                return output;
+            }
+            
+
+        
+
+        public static int InsertCategory(string category, out string errorMessage)
+        {
+            errorMessage = "";
+            SqlConnection dbConnection = new SqlConnection { ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=MonsterPlan;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False" };
+            SqlCommand cmd = new SqlCommand("spCreateCategoryFromName", dbConnection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("name", SqlDbType.NVarChar, 20).Value = category;
+
+            try
+            {
+                dbConnection.Open();
+                int i = 0;
+                i = cmd.ExecuteNonQuery();
+                if (i >= 1)
+                    errorMessage = "";
+                else
+                {
+                    errorMessage = "Could not create category.";
+                }
+                cmd.Dispose();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+                return 0;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
         public static int GetCategory(string category)
         {
             List<TblPosts> postList = new List<TblPosts>();
@@ -66,7 +143,7 @@ namespace Forum.Models
             {
                 conn = new SqlConnection(@"Data Source=(localdb)\mssqllocaldb;Initial Catalog=MonsterPlan;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
                 conn.Open();
-                SqlCommand cmd = new SqlCommand("spGetCategoryIDFromCategory", conn);
+                SqlCommand cmd = new SqlCommand("spGetCategories", conn);
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.Add(new SqlParameter("@category", category));
                 rdr = cmd.ExecuteReader();
